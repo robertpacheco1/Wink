9b93bc874d2256bb7fd6898baa648ae7a702776f
PacMan <PacMan@github.com>
2019-07-11 19:44:15 -0700

    Initial Commit

---
+++ Wink/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Wink
+//
+//  Created by Robert Pacheco on 7/11/19.
+//  Copyright © 2019 Robert Pacheco. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
